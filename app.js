e8b2ec8b95edd0e74df460e8e20b209d2b309e5c
ekalbers <ekalbers22@gmail.com>
2023-02-11 22:57:34 -0600

    finished problems 3 and 4 for step 10

--- app.js
+++ app.js
@@ -132,22 +132,26 @@ let objectLit = () => ({ key1: 'value1', key2: 'value2', key3: 'value3', });
 console.log(objectLit());
 
 
-let sumAndProduct = function (a, b) {
+/* let sumAndProduct = function (a, b) {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
-};
+}; */
+
+let sumAndProduct = (a, b) => [a + b, a * b];
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
+console.log(sumAndProduct(3, 9));
 
 
-let message = function (name) {
+/* let message = function (name) {
   return `Hello, ${name}!`;
-};
+}; */
+
+let message = (name) => `Hello, ${name}!`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
+console.log(message('Allie'));
 
 
 let Student = function (name, age, hometown) {
